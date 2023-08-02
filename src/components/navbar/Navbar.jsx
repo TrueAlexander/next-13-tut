d1bfff9e8f5c12558cc386d6cb01074eecea251c
Alexander <caminante.msk@gmail.com>
2023-08-01 23:58:07 -0300

    finished tutorial

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,10 @@
-"use client"
-import Link from "next/link"
-import styles from "./navbar.module.css"
-import DarkModeToggle from "../DarkModeToggle/DarkModeToggle"
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import styles from "./navbar.module.css";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import { signOut, useSession } from "next-auth/react";
 
 const links = [
   {
@@ -34,22 +37,31 @@ const links = [
     title: "Dashboard",
     url: "/dashboard",
   },
-]
+];
 
 const Navbar = () => {
+  const session = useSession();
+
   return (
     <div className={styles.container}>
-      <Link href="/" className={styles.logo} >
-        TrueAlex
+      <Link href="/" className={styles.logo}>
+        lamamia
       </Link>
       <div className={styles.links}>
-        <DarkModeToggle/>
-        {links.map(link => 
-          <Link className={styles.link} key={link.id} href={link.url}>{link.title}</Link>)}
-        <button className={styles.logout} onClick={() => {console.log("logged out")}} >Logout</button>  
+        <DarkModeToggle />
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
+        ))}
+        {session.status === "authenticated" && (
+          <button className={styles.logout} onClick={signOut}>
+            Logout
+          </button>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
